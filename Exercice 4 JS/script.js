322828daa3285c2285d3fa9c87174422addd65b9
TimCauss <t.caussignac@gmail.com>
2024-04-12 12:00:21 +0200

    adding map

--- Exercice 4 JS/script.js
+++ Exercice 4 JS/script.js
@@ -1,3 +1,11 @@
+let map = L.map("map").setView([51.505, -0.09], 5);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
 async function fetchISS() {
   try {
     const response = await fetch("http://api.open-notify.org/iss-now.json");
